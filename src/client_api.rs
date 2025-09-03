89f7b866157120f57fb19ef4778fe53c65a7132a
SuperKevinZhou <zzh__2020@outlook.com>
2025-09-03 20:13:55 +0800

    Add pgp module

--- src/client_api.rs
+++ src/client_api.rs
@@ -1,9 +1,14 @@
-use std::{collections::HashMap, sync::{Arc, Mutex}};
+use std::{collections::HashMap, io::Cursor, sync::{Arc, Mutex}};
 
 use axum::{
     routing::get,
     extract::{Query, State},
     Router
+};
+use pgp::{
+    composed::{
+        Deserializable, SignedPublicKey
+    }, types::KeyDetails
 };
 
 #[derive(Clone)]
@@ -13,13 +18,25 @@ struct AppState {
 }
 
 async fn login(Query(params): Query<HashMap<String, String>>, State(state): State<AppState>) -> String {
-    if !params.contains_key("user_name") || !params.contains_key("user_uuid") {
+    if !params.contains_key("user_name") || !params.contains_key("user_uuid") || !params.contains_key("user_pgp"){
         "Error: `user_name` and `user_uuid` not given".to_string()
     } else {
+        let user_pgp = urlencoding::decode(params.get(&"user_pgp".to_string()).unwrap()).unwrap().to_string();
         *state.user_data_info.lock().unwrap().entry("user_name".to_string()).and_modify(|e| *e = params.get(&"user_name".to_string()).unwrap().to_string()).or_insert_with(|| params.get(&"user_name".to_string()).unwrap().to_string()) = params.get(&"user_name".to_string()).unwrap().to_string();
         *state.user_data_info.lock().unwrap().entry("user_uuid".to_string()).and_modify(|e| *e = params.get(&"user_uuid".to_string()).unwrap().to_string()).or_insert_with(|| params.get(&"user_uuid".to_string()).unwrap().to_string()) = params.get(&"user_uuid".to_string()).unwrap().to_string();
+        state.user_data_info.lock().unwrap().entry("user_pgp".to_string()).and_modify(|e| *e = user_pgp.clone()).or_insert_with(|| user_pgp.clone());
         *state.signed_in.lock().unwrap() = true;
-        "Login successful".to_string()
+
+        println!("{}", user_pgp);
+
+        let cursor= Cursor::new(user_pgp);
+
+        if let Ok((public_key, _headers)) = SignedPublicKey::from_armor_single(cursor) {
+            let fingerprint = public_key.fingerprint();
+            format!("Login successful, PGP fingerprint is {}", fingerprint)
+        } else {
+            "Public key not found".to_string()
+        }
     }
 }
 
